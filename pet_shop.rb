c8a6f083ab195850f4133d8ffc4243936565e2ed
Rhys Brame <rhysbrame@gmail.com>
2017-03-11 12:12:29 +0000

    comlpeted test 3

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,3 +1,13 @@
 def pet_shop_name(pet_shop)
   return pet_shop[:name]
 end
+
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(pet_shop,added_value)
+  initial_cash = pet_shop[:admin][:total_cash]
+  return pet_shop[:admin][:total_cash] = initial_cash + added_value
+end
+
