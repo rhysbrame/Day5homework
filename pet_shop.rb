9b39577411964768d44fdfb50c0ddd0bbd4dd737
Rhys Brame <rhysbrame@gmail.com>
2017-03-11 12:30:29 +0000

    added 7 passing tests

--- pet_shop.rb
+++ pet_shop.rb
@@ -11,3 +11,15 @@ def add_or_remove_cash(pet_shop,added_value)
   return pet_shop[:admin][:total_cash] = initial_cash + added_value
 end
 
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop, new_pets)
+  initial_pets_sold = pet_shop[:admin][:pets_sold]
+  return pet_shop[:admin][:pets_sold] = initial_pets_sold + new_pets
+end
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].length
+end
